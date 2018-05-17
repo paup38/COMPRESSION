af9ce17a2d2f7a8d5e2522abfec23fb74123e578
PauExamen <PauExamen@gmail.com>
2018-05-17 17:01:26 +0200

    First Commit

--- src/LZW.java
+++ src/LZW.java
@@ -1,31 +1,53 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LZW {
 
     public static void compress(InputStream is, OutputStream os) throws Exception {
 
-        Token[] tk = new Token[255];
+
+        List<Token> list = new ArrayList<>();
 
 
         int current;
-        int index = -1;
+        int index = 0;
 
         while ((current = is.read()) != -1) {
-            int ind = search(tk, current, index);
+            int ind = search(list, (byte) current, index);
 
-
-
-
-
+            if (ind == -1){
+                list.add(new Token(index, (byte) current));
+                index = 0;
+            }else {
+                index = ind;
+            }
         }
+
+
     }
 
-    private static int search(Token[] tokens, int i, int index) {
-        for (int j = 0; j < tokens.length; j++) {
-
+    private static int search(List<Token> list, byte symbol, int index) {
+        for (int j = 0; j < list.size(); j++) {
+            if (list.get(j).getValor() == symbol && list.get(j).getIndex() == index) {
+                return j+1;
+            }
         }
+
+        return -1;
+
+
+
+        /*for (int j = 1; j < tokens.length; j++) {
+            if (i == tokens[j].getValor() && tokens.length == 0){
+                index = j;
+                return index;
+            }else {
+                return -1;
+            }
+        }*/
 
     }
 
--- src/Token.java
+++ src/Token.java
@@ -1,32 +1,38 @@
 public class Token {
 
-        private int index;
-        private int valor;
+    private int index;
+    private byte valor;
 
 
+    public Token(int index, byte valor) {
+        this.index = index;
+        this.valor = valor;
+    }
 
-        public Token (int index, int valor) {
-            this.index = index;
-            this.valor = valor;
-        }
+    @Override
+    public String toString() {
+        return "" + this.index + " " + this.valor;
+    }
 
-        @Override
-        public String toString() {
-            return "" + this.index + " " + this.valor;
-        }
+    @Override
+    public boolean equals(Object o) {
+        Token k = (Token) o;
+        return this.index == k.index && this.valor == k.valor;
+    }
 
-        @Override
-        public boolean equals(Object o) {
-            Token k = (Token) o;
-            return this.index == k.index && this.valor == k.valor;
-        }
+    public int getIndex() {
+        return index;
+    }
 
-        public int getIndex() {
-            return index;
-        }
+    public int getValor() {
+        return valor;
+    }
 
-        public int getValor() {
-            return valor;
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
+    public void setValor(byte valor) {
+        this.valor = valor;
     }
 }
